8ad9da7de31d8e80b6904111cb9b858b59422d0b
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-05-15 04:06:33 +0700

    feat(jobsheet 10): tugas

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\UserModel;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return UserModel::all();
+    }
+
+    public function store(Request $request)
+    {
+        $fields = $request->all();
+        if (isset($fields['password'])) {
+            $fields['password'] = bcrypt($fields['password']);
+        }
+        $user = UserModel::create(
+            $fields
+        );
+        return response()->json($user, 201);
+    }
+
+    public function show(UserModel $user)
+    {
+        return UserModel::find($user);
+    }
+
+    public function update(Request $request, UserModel $user)
+    {
+        $fields = $request->all();
+        if (isset($fields['password'])) {
+            $fields['password'] = bcrypt($fields['password']);
+        }
+        $user->update($fields);
+        return UserModel::find($user);
+    }
+
+    public function destroy(UserModel $user)
+    {
+        $user->delete();
+        return response()->json([
+            'success' => true,
+            'message' => 'Data terhapus'
+        ], 200);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,9 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RegisterController;
 use App\Http\Controllers\Api\LoginController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\BarangController;
+use App\Http\Controllers\Api\KategoriController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,3 +35,36 @@ Route::post('/levels', [App\Http\Controllers\Api\LevelController::class, 'store'
 Route::get('/levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'show']);
 Route::put('/levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'update']);
 Route::delete('/levels/{level}', [App\Http\Controllers\Api\LevelController::class, 'destroy']);
+
+Route::group(
+    ['prefix' => 'users', 'middleware' => 'auth:api'],
+    function () {
+        Route::get('/', [UserController::class, 'index']);
+        Route::post('/', [UserController::class, 'store']);
+        Route::get('/{user}', [UserController::class, 'show']);
+        Route::put('/{user}', [UserController::class, 'update']);
+        Route::delete('/{user}', [UserController::class, 'destroy']);
+    }
+);
+
+Route::group(
+    ['prefix' => 'barang', 'middleware' => 'auth:api'],
+    function () {
+        Route::get('/', [BarangController::class, 'index']);
+        Route::post('/', [BarangController::class, 'store']);
+        Route::get('/{barang}', [BarangController::class, 'show']);
+        Route::put('/{barang}', [BarangController::class, 'update']);
+        Route::delete('/{barang}', [BarangController::class, 'destroy']);
+    }
+);
+
+Route::group(
+    ['prefix' => 'kategori', 'middleware' => 'auth:api'],
+    function () {
+        Route::get('/', [KategoriController::class, 'index']);
+        Route::post('/', [KategoriController::class, 'store']);
+        Route::get('/{kategori}', [KategoriController::class, 'show']);
+        Route::put('/{kategori}', [KategoriController::class, 'update']);
+        Route::delete('/{kategori}', [KategoriController::class, 'destroy']);
+    }
+);
