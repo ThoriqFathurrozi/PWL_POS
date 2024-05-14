e419e6fefd81b2539ecf2918a715dc50c3a84161
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-05-15 02:20:07 +0700

    feat(jobsheet 10): praktikum 3

--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', RegisterController::class)->name('register');
 Route::post('/login', LoginController::class)->name('login');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
