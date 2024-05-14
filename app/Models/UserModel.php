bf3be4209780270403b7f0090ca471dbab189a71
Thoriq Fathurrozi <thoriqfathurrozi@gmail.com>
2024-05-15 00:50:52 +0700

    feat: Praktikum Jobsheet 9

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -2,12 +2,15 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Auth\Authenticatable as AuthenticatableTrait;
 
-class UserModel extends Model
+class UserModel extends Model implements Authenticatable
 {
+    use AuthenticatableTrait;
     use HasFactory;
     protected $table = 'm_user'; //m_user is the table name
     protected $primaryKey = 'user_id'; //user_id is the primary key
